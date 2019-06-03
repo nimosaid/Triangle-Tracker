9c3ecea0f54a1f301166211f2ee8ad08b0ae254e
nimosaid <saidnimo123@gmail.com>
2019-06-03 10:21:01 +0300

    added a submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,14 @@
-function getResult(){
-var hypotenuse= parseFloat(document.getElementById("side1").value);
-var opposite = parseFloat(document.getElementById("side2").value);
-var adjacent = parseFloat(document.getElementById("side3").value);
-var determine = function(side1,side2, adjacent){
-if (side1 === side2 && side2 === side3 && side1 === side3){
-alert("Equilateral");
-}
-
-else if ((side1 === side2 && side2 === side3) (side1 != side3 && side2 !== side3));
-alert("Isosceles");
-
-}
-determine();
-if(side1 === side2 && side1 === side1 && side2 === side3){
-alert("Equilateral");
-}
-else if(side1 === side2 || side1 === adjacent || side2 === side3){
-alert("Isosceles");
-}
-else if((side1 + side2) <= adjacent || (side1 + side3) <=side2 || (side2 + side3) <= side1) {
-alert("Not a definite triangle");
-}
-else{
-alert("Scalene");
-}
-}
+function getResult() {
+  var side1 = parseFloat(document.getElementById("side1").value);
+  var side2 = parseFloat(document.getElementById("side2").value);
+  var side3 = parseFloat(document.getElementById("side3").value);
+    if (side1 === side2 && side2 === side3 && side1 === side3) {
+      alert("Equilateral");
+    } else if ((side1 === side2 && side1 !== side3) || (side1 === side3 && side1 !== side2) || (side2 === side3 && side2 !== side1)) {
+      alert("Isosceles");
+    } else if ((side1 + side2) < side3 || (side1 + side3) < side2 || (side2 + side3) < side1) {
+      alert("Not a definite triangle");
+    } else {
+      alert("Scalene");
+    }
+};
